58ff5fec1eaa3020b64c81d44f261e63633d3963
Joona Oja <n0ojjo00@students.oamk.fi>
2022-02-25 14:15:35 +0200

    status messages updated, no end game yet, also missing point calculation

--- components/Gameboard.js
+++ components/Gameboard.js
@@ -1,9 +1,9 @@
 import { View, Text, Pressable } from 'react-native';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Col, Row, Grid } from "react-native-easy-grid";
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import Styles from '../styles/Styles';
-import { getApplication } from 'react-native-web/dist/cjs/exports/AppRegistry/renderApplication';
+
 
 const POINT_SLOTS = 6;
 const DICES = 5;
@@ -16,7 +16,7 @@ for (let i = 0; i < POINT_SLOTS; i++) {
         {
             id: i,
             selected: false,
-            locked: false,
+            locked: true,
             value: i+1,
             sum: 0,
             icon: "numeric-" + [i + 1] + "-box-multiple-outline"
@@ -33,21 +33,44 @@ const [selectedDices, setSelectedDices] =
     useState(new Array(DICES).fill(false));
 const [selectedPoints, setSelectedPoints] =
     useState(slots);
+    const [gameStarted, setGameStarted] = useState(false);
 
 // const [selectedPoints, setSelectedPoints] =
 //     useState(new Array(POINTS).fill(false));
 
+useEffect(() => {
+    if (gameStarted === false) {
+        setStatus("Game has not started");
+    }
+    if (throwsLeft == 0) {
+        let dices = [...selectedDices];
+        dices.fill(false);
+        setSelectedDices(dices);
+        setStatus("Select points to keep.")
+    }
+    if (throwsLeft < 0) {
+        setThrowsLeft(THROWS - 1);
+    }
+}, [throwsLeft]);
 
+
+// FUNCTION: get the index of slots object
+const getIndex = (i) => {
+    let index = selectedPoints.findIndex((item => item.id == i));
+    return index;
+}
 
 // FUNCTION: THROW DICE
 const throwDice = () => {
-    
+
     for (let i = 0; i < DICES; i++) {
         if (!selectedDices[i]) {
             let randomNumber = Math.floor(Math.random() * 6 + 1 );
             board[i] = 'dice-' + randomNumber;
         }
     }
+    setGameStarted(true);
+    setStatus("Keep playing");
     setThrowsLeft(throwsLeft - 1 )
     console.log(selectedPoints);
     // console.log(board);
@@ -55,6 +78,13 @@ const throwDice = () => {
 
 // FUNCTION: SELECT DICE TO KEEP
 const selectDice = (i) => {
+    if (throwsLeft == 0) {
+        setStatus("Select points to keep before next round!");
+        return;
+    } else if (throwsLeft == 3) {
+        setStatus("Throws the first throw before keeping dice!");
+        return;
+    }
     let dices = [...selectedDices];
     dices[i] = selectedDices[i] ? false : true;
     setSelectedDices(dices);
@@ -62,25 +92,32 @@ const selectDice = (i) => {
 
 // FUNCTION: SET COLOR OF SELECTED DICE
 const getDiceColor = (i) => {
-    return selectedDices[i] ? "red" : "black";
+    return selectedDices[i] ? "black" : "red";
 }
 
 // FUNCTION: SELECT POINTS TO KEEP
 const selectPoints = (i) => {
+    if (throwsLeft > 0) {
+        setStatus("Throw your dice first!");
+        return;
+    }
     let points = [...selectedPoints];
     // points[i] = selectedPoints[i] ? false : true;
     // setSelectedPoints(points);
-    let index = points.findIndex((item => item.id == i));
+    let index = getIndex(i);
     if (points[index]["selected"] == true) {
         return;
     } else {
         points[index]["selected"] = true;
         setSelectedPoints(points);
+        setThrowsLeft(THROWS);
+        setStatus("Begin the next round");
+        // throwDice();
     }
 }
 
 const getIcon = (i) => {
-    let index = selectedPoints.findIndex((item => item.id == i));
+    let index = getIndex(i);
     let iconToReturn = selectedPoints[index]["icon"];
     return iconToReturn;
 }
@@ -88,7 +125,7 @@ const getIcon = (i) => {
 // FUNCTION: SET SELECTED POINTS CATEGORY COLOR
 const getPointsColor = (i) => {
     //  return selectedPoints[i] ? "red" : "black";
-    let index = selectedPoints.findIndex((item => item.id == i));
+    let index = getIndex(i);
     if (selectedPoints[index]["selected"] === true) {
         return "black";
     } else {
@@ -140,10 +177,11 @@ for (let i = 0; i < slots.length; i++) {
         <Col>
             <Row size={2}><Col/>{row}<Col/></Row>
             <Row><Text>Throws Left: {throwsLeft}</Text></Row>
-            <Row><Text>Game Status Message</Text></Row>
+            <Row><Text>Game status: {status}</Text></Row>
             <Row size={2} style={Styles.row}>
                 <Pressable
                 style={Styles.button}
+                disabled={throwsLeft == 0 ? true : false}
                 onPress={() => throwDice()}>
                     <Text style={Styles.buttonText}>Throw dice</Text>
                 </Pressable>
